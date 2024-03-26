53a642193befd7341c19de59afefdffe43c38afa
Enrique <elacaveruiz@safareyes.es>
2024-03-26 14:16:58 +0100

    enr - primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
