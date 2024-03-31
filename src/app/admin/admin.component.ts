6df8670528d86d6fd472892804de547ee86a449d
Enrique <elacaveruiz@safareyes.es>
2024-04-01 01:51:15 +0200

    enr - 31/03/24

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,29 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
-  albumes: any[] = [];
+export class AdminComponent {
 
-  constructor(
-    private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.obtenerAlbumes();
-  }
-
-  obtenerAlbumes(): void {
-    this.http.get<any[]>('http://localhost:8080/album').subscribe(
-      albumes => {
-        this.albumes = albumes;
-      },
-      error => {
-        console.error('Error al obtener datos:', error);
-      }
-    );
-  }
 }
--- src/app/admin/album/album.component.ts
+++ src/app/admin/album/album.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-album',
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.css']
 })
-export class AlbumComponent {
+export class AlbumComponent implements OnInit {
 
+  albumes: any[] = [];
+
+  constructor(
+    private http: HttpClient) { }
+
+
+  ngOnInit(): void {
+    this.obtenerAlbumes();
+  }
+
+  obtenerAlbumes(): void {
+    this.http.get<any[]>('http://localhost:8080/album').subscribe(
+      albumes => {
+        this.albumes = albumes;
+      },
+      error => {
+        console.error('Error al obtener datos:', error);
+      }
+    );
+  }
 }
